ac0f602ad35b0989f1d88dc08d069c2c23b7c4df
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-03-01 14:48:14 +0100

    Made the first menu work as I wanted it to with a while loop and switch-statement

--- Program.cs
+++ Program.cs
@@ -5,23 +5,42 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to Main menu. Please enter selected number for wanted action:" +
-                "\n1. Buy movietickets " +
-                "\n2. You want to repeat yourself " +
-                "\n3. Play the third word-Game " +
-                "\n0. Exit application");
 
+            Console.WriteLine("Welcome to Main menu. Please enter selected number for wanted action:" +
+               "\n1. Buy movietickets " +
+               "\n2. You want to repeat yourself " +
+               "\n3. Play the third word-Game " +
+               "\n0. Exit application");
             string selectedAction = Console.ReadLine();
 
             //Menu creation
-            switch(selectedAction){
-                case "0":
-                    Console.WriteLine("Exiting application");
-                    break;
-                default:
-                    Console.WriteLine("Invalid input");
-                    break;
+            while (selectedAction != "0") {
+                switch (selectedAction)
+                {
+                    case "1":
+                        Console.WriteLine("Number one");
+                        break;
+                    case "2":
+                        Console.WriteLine("Number two");
+                        break;
+                    case "3":
+                        Console.WriteLine("Number three");
+                        break;
+                    case "0":
+                        Console.WriteLine("Exiting application");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+                Console.WriteLine("Welcome to Main menu. Please enter selected number for wanted action:" +
+               "\n1. Buy movietickets " +
+               "\n2. You want to repeat yourself " +
+               "\n3. Play the third word-Game " +
+               "\n0. Exit application");
+                selectedAction = Console.ReadLine();
             }
+           
         }
     }
 }
