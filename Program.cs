2607adebcd284cc95f6980d721bd924d2b9a98c9
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-03-01 12:07:55 +0100

    Made first main menu for project with two cases

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Main menu. Please enter selected number for wanted action: ");
+            
+            
+            Console.WriteLine("0. Exit program");
+
+            string selectedAction = Console.ReadLine();
+
+            //Menu creation
+            switch(selectedAction){
+                case "0":
+                    Console.WriteLine("Exiting application");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
         }
     }
 }
